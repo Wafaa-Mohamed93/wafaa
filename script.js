9807f17c29cbb1070224ee49865c7c191a5f1a22
Wafaa-Mohamed93 <wafaamoh152@gmail.com>
2025-07-28 04:55:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// JavaScript for language detection will be here
+// كود يحدد لغة المتصفح ويوجه المستخدم تلقائيًا للنسخة المناسبة
+const userLang = navigator.language || navigator.userLanguage;
+
+if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/") {
+  if (userLang.startsWith("ar")) {
+    window.location.href = "index-ar.html";
+  } else {
+    window.location.href = "index-en.html";
+  }
+}
